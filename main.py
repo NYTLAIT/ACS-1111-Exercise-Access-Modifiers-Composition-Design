765900c113d9ec9acb41a566763cb5773b01a6d4
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-02-10 15:48:57 -0800

    Starting Stretch Challenges: implementing a new deck so i can check out other functions

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
 import random
 
 class Card:
-    def __init__(self, suit, value, color):
+    def __init__(self, name, suit, value, color):
+        self.name = name
         self.suit = suit
         self.value = value
         self.color = color
 
     def show(self):
-        print(f'Value: {self.value} Suit: {self.suit}')
+        print(f'Name: {self.name} Suit: {self.suit}')
 
 class Deck:
     def __init__(self, cards=[]):
         self.cards = cards
+        self.discard_pile = []
+
+    def fresh_deck(self):
+        fresh_deck = []
+        suits_colors = [
+            {"suit": "hearts", "color": "red"},
+            {"suit": "clubs", "color": "black"},
+            {"suit": "diamonds", "color": "red"},
+            {"suit": "spades", "color": "black"}
+            ]
+        names_values = [()]
+
+        # names = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+
+        # for suit_color in suits_colors:
+        #     for i in range(13):
+        #         card = Card(names[i], suit_color["suit"], i + 1, suit_color["color"])
+        #         fresh_deck.append(card)
+        # self.cards = fresh_deck
 
     def add_card(self, card):
         self.cards.append(card)
 
     def shuffle(self):
         random.shuffle(self.cards)
+
+    def draw(self):
+        draw = random.randint(0, len(self.cards) - 1)
+        self.cards[draw].show()
+
+    def discard(self):
+        pass
+    
+
+        
